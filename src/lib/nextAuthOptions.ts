1b852120ce010c23a0d49bed9dafc155a3051745
Farhan <farhanisawake@gmail.com>
2024-05-11 16:46:02 +0600

    init

---
+++ src/lib/nextAuthOptions.ts
@@ -0,0 +1,28 @@
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider, { GithubProfile } from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import DiscordProvider, { DiscordProfile } from "next-auth/providers/discord";
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth",
+    error: "/error",
+  },
+  debug: true,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+  ],
+
+
+};
